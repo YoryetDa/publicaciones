1833853d7be878b4efa25b39420cb1f2b4cd2a60
yoryet <y.danoun@pharmatender.cl>
2024-04-13 22:32:34 -0400

    add config Conexion bd Oracle Cloud DDL = None

--- src/main/java/com/publicacion/publicacion/PublicacionApplication.java
+++ src/main/java/com/publicacion/publicacion/PublicacionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PublicacionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PublicacionApplication.class, args);
 	}
