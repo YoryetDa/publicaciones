96d4a865249292189090c2da90ee161888463809
yoryet <y.danoun@pharmatender.cl>
2024-04-14 14:29:39 -0400

    fix : add findByPublicacionId to import from comentario controller

--- src/main/java/com/publicacion/publicacion/repositories/ComentarioRepository.java
+++ src/main/java/com/publicacion/publicacion/repositories/ComentarioRepository.java
@@ -2,6 +2,8 @@ package com.publicacion.publicacion.repositories;
 
 import com.publicacion.publicacion.models.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+    List<Comentario> findByPublicacionId(Long publicacionId);
+
 }
