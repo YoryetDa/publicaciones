63226ff52a36e6db21bb618c587f84a45085f184
yoryet <y.danoun@pharmatender.cl>
2024-04-14 01:13:09 -0400

    feat: add model comentario

---
+++ src/main/java/com/publicacion/publicacion/models/Comentario.java
@@ -0,0 +1,37 @@
+package com.publicacion.publicacion.models;
+
+
+import jakarta.persistence.*;;
+
+
+@Entity
+@Table(name = "comentarios")
+public class Comentario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String autor;
+    private String mensaje;
+    private Integer nota;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "publicacion_id")
+    private Publicacion publicacion;
+
+    public Comentario() {
+    }
+
+    // Getters y setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+    public String getMensaje() { return mensaje; }
+    public void setMensaje(String mensaje) { this.mensaje = mensaje; }
+    public Integer getNota() { return nota; }
+    public void setNota(Integer nota) { this.nota = nota; }
+    public Publicacion getPublicacion() { return publicacion; }
+    public void setPublicacion(Publicacion publicacion) { this.publicacion = publicacion; }
+}
