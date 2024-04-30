d57f617f6d4b114857a07a8c8dd142c1b4b7168c
yoryet <y.danoun@pharmatender.cl>
2024-04-30 00:19:30 -0400

    add test link

--- src/test/java/com/publicacion/publicacion/PublicacionApplicationTests.java
+++ src/test/java/com/publicacion/publicacion/PublicacionApplicationTests.java
@@ -2,15 +2,32 @@ package com.publicacion.publicacion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.BeforeEach;
 @SpringBootTest
 public class PublicacionApplicationTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-    @Test
-    void contextLoads() {
-        // Este test simplemente arranca el contexto de la aplicación.
-        // Si hay algún problema de configuración que impida que la aplicación se inicie,
-        // este test fallará.
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+    
+    
+        @Test
+    public void testPublicacionLinks() throws Exception {
+        mockMvc.perform(get("/publicaciones"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$._embedded.publicaciones[0]._links.self.href").exists());
+    }
     }
 
-}
